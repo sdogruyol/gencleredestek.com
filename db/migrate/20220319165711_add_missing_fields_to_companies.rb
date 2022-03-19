7c08ad530b1756091e06b6cfba9edfa424f07a44
Serdar Dogruyol <990485+sdogruyol@users.noreply.github.com>
2022-03-19 19:58:12 +0300

    Add missing fields to Company

---
+++ db/migrate/20220319165711_add_missing_fields_to_companies.rb
@@ -0,0 +1,8 @@
+class AddMissingFieldsToCompanies < ActiveRecord::Migration[7.0]
+  def change
+    add_column :companies, :website, :string
+    add_column :companies, :location, :string
+    add_column :companies, :perks, :string
+    add_column :companies, :work_type, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_19_164257) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_19_165711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_19_164257) do
     t.string "contact_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "website"
+    t.string "location"
+    t.string "perks"
+    t.string "work_type"
   end
 
 end
