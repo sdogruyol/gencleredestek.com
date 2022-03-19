a6cae82d199a6bbfadbb94d4a68bcd2c17900e86
Serdar Dogruyol <990485+sdogruyol@users.noreply.github.com>
2022-03-19 19:27:23 +0300

    Add company model and index page

---
+++ db/migrate/20220319161857_create_companies.rb
@@ -0,0 +1,11 @@
+class CreateCompanies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :description
+      t.string :contact_email
+
+      t.timestamps
+    end
+  end
+end
