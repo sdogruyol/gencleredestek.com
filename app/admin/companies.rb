289ab0f0249181e62e4d1e2bb185675e3bf5a111
Serdar Dogruyol <990485+sdogruyol@users.noreply.github.com>
2022-03-24 10:04:07 +0300

    Add work type to Activeadmin

--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register Company do
-  permit_params :name, :contact_email, :description, :location, :perks, :website, :work_type
+  permit_params :name, :contact_email, :description, :location, :perks, :website, :work_type,
+                work_types_attributes: [:id, :name, :_destroy]
+
+  form do |f|
+    f.inputs 'Details' do
+      f.input :name
+      f.input :description
+      f.input :location
+      f.input :perks
+    end
+
+    f.inputs 'Work Types' do
+      f.has_many :work_types, allow_destroy: true, new_record: false do |b|
+        b.input :name, label: 'Work Type', as: :select, collection: WorkType.all
+      end
+    end
+
+    f.actions
+  end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ApplicationRecord
   has_and_belongs_to_many :work_types
+
+  accepts_nested_attributes_for :work_types
 end
