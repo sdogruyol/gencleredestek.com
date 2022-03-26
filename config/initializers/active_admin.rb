64ce9c600aa488b14079ddaf8270938fd51155e9
Serdar Dogruyol <990485+sdogruyol@users.noreply.github.com>
2022-03-26 15:35:05 +0300

    Tweak ActiveAdmin config

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -124,7 +124,7 @@ ActiveAdmin.setup do |config|
   # This allows your users to comment on any resource registered with Active Admin.
   #
   # You can completely disable comments:
-  # config.comments = false
+  config.comments = false
   #
   # You can change the name under which comments are registered:
   # config.comments_registration_name = 'AdminComment'
@@ -271,7 +271,7 @@ ActiveAdmin.setup do |config|
   # Pagination is enabled by default for all resources.
   # You can control the default per page count for all resources here.
   #
-  # config.default_per_page = 30
+  config.default_per_page = 50
   #
   # You can control the max per page count too.
   #
