9ab90a7e25e33a9ae02f30ffcd4513dae82380d7
nejdetkadir <nejdetkadir.550@gmail.com>
2022-03-28 23:14:06 +0300

    Fix tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get home_index_url
+    get root_path
     assert_response :success
   end
 end
