fd1a70a37b0fe2165d68db909a8eaf94910df405
Serdar Dogruyol <990485+sdogruyol@users.noreply.github.com>
2025-05-08 16:42:22 +0300

    Fix admin_user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -16,4 +16,8 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[id email created_at updated_at]
+  end
 end
